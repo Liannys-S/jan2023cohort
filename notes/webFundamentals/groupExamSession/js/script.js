b30b8d1206ea45e6658dcab9f33aac2e2082a054
Liannys Sanchez <sanchezliannys@gmail.com>
2023-02-06 11:40:19 -0600

    group project

--- notes/webFundamentals/groupExamSession/js/script.js
+++ notes/webFundamentals/groupExamSession/js/script.js
@@ -1,10 +1,12 @@
 console.log('Wired up')
 
 function changeImg(element) {
-    // console.log('activated mouse event')
+    console.log('activated mouse event')
+    element.src = "./images/yankeeBoy.png"
 }
 function changeBack(element) {
     // console.log('new event')
+    element.src ="./images/breezee.png"
 }
 let count = 0;
 function addToCart(element) {
@@ -22,6 +24,14 @@ function addToCart(element) {
 
 function salePrices(element) {
     // console.log('button click')
-    document.getElementById('sale')
-
+    document.getElementById('sale').style.display = "none"
+    let price = document.getElementsByClassName('price')
+    console.log(price)
+    for(let i=0;i<price.length;i++){
+        let temp = price[i].innerText
+        // console.log(price[i].innerText)
+        temp = temp-(temp*0.2)
+        // console.log(temp)
+        price[i].textContent = temp
+    }
 }
