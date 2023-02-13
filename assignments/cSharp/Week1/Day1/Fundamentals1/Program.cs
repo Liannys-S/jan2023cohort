27eaa827137477595fd2ae196f0cc5af5ee8aaff
Liannys Sanchez <sanchezliannys@gmail.com>
2023-02-13 12:04:06 -0600

    modified

--- assignments/cSharp/Week1/Day1/Fundamentals1/Program.cs
+++ assignments/cSharp/Week1/Day1/Fundamentals1/Program.cs
@@ -1,39 +1,29 @@
-﻿using System;
+﻿
+// loop that prints values 1-255
+for(int i = 1; i <= 255; i++){
+    Console.WriteLine(i);
+}
 
-namespace Fundamentals1 
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            // loop that prints values 1-255
-            for(int i = 1; i <= 255; i++){
-                Console.WriteLine(i);
-            }
+// loop that generates 5 random numbers and sums them up
+Random rand = new Random();
+int sum = 0;
+for(int i = 0; i < 5; i++){
+    int temp = rand.Next(10,21);
+    Console.WriteLine(temp);
+    sum += temp;
+}
+Console.WriteLine(sum);
 
-            // loop that generates 5 random numbers and sums them up
-            Random rand = new Random();
-            int sum = 0;
-            for(int i = 0; i < 5; i++){
-                int temp = rand.Next(10,21);
-                Console.WriteLine(temp);
-                sum += temp;
-            }
-            Console.WriteLine(sum);
-
-            // loop that prints values 1-100 that are divisible by 3 or 5 but not both
-            for(int i = 1; i <= 100; i++){
-                if((i % 3 == 0 || i % 5 == 0) && !(i % 3 == 0 && i % 5 == 0)){
-                    Console.WriteLine(i);
-                    if(i % 3 == 0){
-                        Console.WriteLine("Fizz");
-                    }else{
-                        Console.WriteLine("Buzz");
-                    }
-                } else if(i % 3 == 0 && i % 5 == 0){
-                    Console.WriteLine("FizzBuzz");
-                }
-            }
+// loop that prints values 1-100 that are divisible by 3 or 5 but not both
+for(int i = 1; i <= 100; i++){
+    if((i % 3 == 0 || i % 5 == 0) && !(i % 3 == 0 && i % 5 == 0)){
+        Console.WriteLine(i);
+        if(i % 3 == 0){
+            Console.WriteLine("Fizz");
+        }else{
+            Console.WriteLine("Buzz");
         }
+    } else if(i % 3 == 0 && i % 5 == 0){
+        Console.WriteLine("FizzBuzz");
     }
 }
