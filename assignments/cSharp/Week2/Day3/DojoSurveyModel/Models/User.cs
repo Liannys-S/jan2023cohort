be2395f753cefefcdbe8e976f6b87f87f28fa6bf
Liannys Sanchez <sanchezliannys@gmail.com>
2023-02-23 23:07:01 -0600

    Custom Validation

--- assignments/cSharp/Week2/Day3/DojoSurveyModel/Models/User.cs
+++ assignments/cSharp/Week2/Day3/DojoSurveyModel/Models/User.cs
@@ -14,5 +14,21 @@ public class User
     [MinLength(20, ErrorMessage ="Comment must be at least 20 characters")]
     public string? Comment{ get; set; }
 
+    [DataType(DataType.Date)]
+    [FutureDate]
+    public DateTime Birthday{get; set; }
     
 }
+public class FutureDateAttribute : ValidationAttribute
+{
+    protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+    {
+        if(((DateTime)value) >= DateTime.Now.Date){
+            return new ValidationResult("Date entered must before today's date");
+        }else{
+            return ValidationResult.Success;
+        }
+        // You first may want to unbox "value" here and cast to to a DateTime variable!
+    }
+}
+
